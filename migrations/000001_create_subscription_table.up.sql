2378e756e2870e9d06d6f0f6d4c46c3c1f3866bf
KTo-9l <konstantinov.andrei03@mail.ru>
2025-11-10 22:58:49 +0500

    update migration.up

--- migrations/000001_create_subscription_table.up.sql
+++ migrations/000001_create_subscription_table.up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS subscription(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     service_name text NOT NULL,
     price integer NOT NULL,
     user_id uuid NOT NULL,
     start_date date NOT NULL,
     end_date date,
-    is_deleted boolean NOT NULL DEFAULT false,
-    CONSTRAINT subscription_pkey PRIMARY KEY (id)
+    is_deleted boolean NOT NULL DEFAULT false
 );
